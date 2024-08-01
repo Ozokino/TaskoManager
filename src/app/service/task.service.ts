3ec156c8854329c34d69ae71ba5a355d147b2928
Ozo <137302407+Ozokino@users.noreply.github.com>
2024-08-01 18:41:59 +0300

    Updated service for fetching tasks from DB. Some things don't work because the app is not yet compatible with the new and glorious service

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,34 +1,52 @@
 import { Injectable } from '@angular/core';
-import { Task } from '../task';
-// import { HttpClient } from '@angular/common/http';
-import tasksdata from '../../assets/tasks.json';//mēģināju ielikt no assets foldera, bet nekādīgi nestrādāja//sanāca
+import { Task, tempTask } from '../../models/task.model';
+import { HttpClient } from '@angular/common/http';
+// import tasksdata from '../../assets/tasks.json';//mēģināju ielikt no assets foldera, bet nekādīgi nestrādāja//sanāca
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private tasks: Task[] = tasksdata.tasks;
-  // tasks: Task[] = [];
-  // constructor() { }private http:HttpClient
+  constructor(private http: HttpClient){}
 
-  getTasks(): Task[] {
-    return this.tasks;
-  }
+  private apiUrl = 'http://localhost:8080/api/tasks';
 
-  addTask(task: Task) {
-    this.tasks.push(task);
-  }
-
-  updateTask(updatedTask: Task){
-    const index = this.tasks.findIndex(task=> task.createdOn=== updatedTask.createdOn);
-    if (index !==-1){
-      this.tasks[index]= updatedTask;
-    }
-  }
-
-  deleteTask(index: number) {
-    this.tasks.splice(index, 1);
-  }
+getAllTasks(){
+  return this.http.get(this.apiUrl);
 }
+getATask (id:string){  
+  return this.http.get(`${this.apiUrl}/${id}`);
+}
+createTask (task :tempTask){  
+  return this.http.post(this.apiUrl, task);
+}
+deleteATask (id:string){  
+  return this.http.delete(`${this.apiUrl}/${id}`);
+}
+editATask (id:string, task:Task){  
+  return this.http.patch(`${this.apiUrl}/${id}`,task);
+}
+}
+ // private tasks: Task[] = tasksdata.tasks;
+  // // tasks: Task[] = [];
+  // // constructor() { }private http:HttpClient
 
+  // getTasks(): Task[] {
+  //   return this.tasks;
+  // }
+
+  // addTask(task: Task) {
+  //   this.tasks.push(task);
+  // }
+
+  // updateTask(updatedTask: Task){
+  //   const index = this.tasks.findIndex(task=> task.createdOn=== updatedTask.createdOn);
+  //   if (index !==-1){
+  //     this.tasks[index]= updatedTask;
+  //   }
+  // }
+
+  // deleteTask(index: number) {
+  //   this.tasks.splice(index, 1);
+  // }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskService } from '../service/task.service';
-import { Task } from '../task';
+import { Task } from '../../models/task.model';
 import { RouterLink } from '@angular/router';
 
 @Component({
